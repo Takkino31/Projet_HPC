cb23c2fdd4f44eab36125dda55a89bf5a16a7a64
Takkino31 <takkinoyaya@gmail.com>
2025-03-04 23:23:43 +0000

    first commit

---
+++ src/parallel/MatrixProduct_parallel_static.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parallel;
+
+/**
+ *
+ * @author dell
+ */
+public class MatrixProduct_parallel_static {
+    
+}
